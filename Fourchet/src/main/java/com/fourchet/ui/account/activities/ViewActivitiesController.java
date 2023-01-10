c666b0ffe57d27470fda450f27eff320889d9a28
teena-nuihau <nuihau.teena@etu.umontpellier.fr>
2023-01-10 22:37:06 +0100

    debut view prepa a merge

---
+++ Fourchet/src/main/java/com/fourchet/ui/account/activities/ViewActivitiesController.java
@@ -0,0 +1,13 @@
+package com.fourchet.ui.account.activities;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewActivitiesController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
