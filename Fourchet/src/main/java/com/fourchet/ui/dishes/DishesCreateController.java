d523bc2a4a0e00f0f60f236e98b9e14eabdd7940
teena-nuihau <nuihau.teena@etu.umontpellier.fr>
2023-01-11 01:05:20 +0100

    fin projet

---
+++ Fourchet/src/main/java/com/fourchet/ui/dishes/DishesCreateController.java
@@ -0,0 +1,11 @@
+package com.fourchet.ui.dishes;
+
+import javafx.event.ActionEvent;
+
+public class DishesCreateController {
+    public void search(ActionEvent actionEvent) {
+    }
+
+    public void Publier(ActionEvent actionEvent) {
+    }
+}
